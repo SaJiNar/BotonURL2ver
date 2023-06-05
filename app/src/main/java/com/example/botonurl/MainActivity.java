e8a3c31f8178599748b9a40793507a76e0934d84
SaJiNar <sarajnares@gmail.com>
2023-06-05 19:45:41 +0200

    Changes to make possible that navigates to another website wrote at URL

--- app/src/main/java/com/example/botonurl/MainActivity.java
+++ app/src/main/java/com/example/botonurl/MainActivity.java
@@ -4,20 +4,33 @@ import android.content.Intent;
 import android.net.Uri;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.EditText;
 
 import androidx.appcompat.app.AppCompatActivity;
 
 public class MainActivity extends AppCompatActivity {
 
+    private EditText urlEditText;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        urlEditText = findViewById(R.id.EditTextURL);
     }
 
-    public void browser (View view) {
-        Intent browserIntent = new Intent(Intent.ACTION_VIEW, Uri.parse("http://google.com"));
-        startActivity(browserIntent);
-    }
+    public void browser(View view) {
+        String url = urlEditText.getText().toString();
 
+        if (!url.isEmpty()) {
+            if (!url.startsWith("http://") && !url.startsWith("https://")) {
+                url = "http://" + url;
+            }
+
+            Intent browserIntent = new Intent(Intent.ACTION_VIEW, Uri.parse(url));
+            startActivity(browserIntent);
+        }
+    }
 }
+
